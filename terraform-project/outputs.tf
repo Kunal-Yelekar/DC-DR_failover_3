ae6015a9696394a45d32393b28d519b7b3c34bf9
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 01:06:13 +0530

    Update outputs.tf

--- terraform-project/outputs.tf
+++ terraform-project/outputs.tf
@@ -9,26 +9,16 @@ output "dr_vpc_id" {
 }
 
 output "dc_alb_dns" {
-  description = "DNS name of the DC ALB"
+  description = "DC ALB DNS Name"
   value       = module.dc_alb.alb_dns_name
 }
 
 output "dr_alb_dns" {
-  description = "DNS name of the DR ALB"
+  description = "DR ALB DNS Name"
   value       = module.dr_alb.alb_dns_name
 }
 
 output "global_accelerator_dns" {
-  description = "DNS name of the Global Accelerator"
-  value       = module.global_accelerator.accelerator_dns_name
-}
-
-output "dc_ec2_instance_ids" {
-  description = "EC2 instance IDs in the DC VPC"
-  value       = module.dc_ec2.instance_ids
-}
-
-output "dr_ec2_instance_ids" {
-  description = "EC2 instance IDs in the DR VPC"
-  value       = module.dr_ec2.instance_ids
+  description = "Global Accelerator DNS Name"
+  value       = module.global_accelerator.ga_dns_name
 }
