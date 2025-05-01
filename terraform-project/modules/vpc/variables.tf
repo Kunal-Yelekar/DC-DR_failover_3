8030ba77662f545891f4a83466737f2e5acbca84
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 01:19:18 +0530

    Update variables.tf

--- terraform-project/modules/vpc/variables.tf
+++ terraform-project/modules/vpc/variables.tf
@@ -1,30 +1,19 @@
-variable "vpc_name" {
-  description = "Name of the VPC"
-  type        = string
-}
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
 
-variable "public_subnets" {
-  description = "List of public subnet CIDRs"
-  type        = list(string)
-}
-
-variable "private_subnets" {
-  description = "List of private subnet CIDRs"
-  type        = list(string)
-  default     = []
+variable "vpc_name" {
+  description = "Name for the VPC"
+  type        = string
 }
 
 variable "availability_zones" {
-  description = "List of availability zones"
+  description = "List of Availability Zones to use for subnets"
   type        = list(string)
 }
 
-variable "environment" {
-  description = "Environment name (e.g. DC or DR)"
-  type        = string
+variable "public_subnets" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
 }
