62a90f2b7ea025d1a20480020bc343fde95780bf
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 00:44:32 +0530

    Update outputs.tf

--- terraform-project/modules/alb/outputs.tf
+++ terraform-project/modules/alb/outputs.tf
@@ -1,7 +1,14 @@
 output "alb_arn" {
-  value = aws_lb.this.arn
+  description = "ARN of the ALB"
+  value       = aws_lb.this.arn
 }
 
-output "dns_name" {
-  value = aws_lb.this.dns_name
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = aws_lb_target_group.this.arn
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the ALB"
+  value       = aws_lb.this.dns_name
 }
