59312fa44cbf3b1b5d59a6e861ef7a395abefd21
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 01:17:15 +0530

    Update main.tf

--- terraform-project/modules/alb/main.tf
+++ terraform-project/modules/alb/main.tf
@@ -1,33 +1,32 @@
 resource "aws_lb" "this" {
-  name               = "${var.environment}-alb"
+  name               = var.alb_name
   internal           = false
   load_balancer_type = "application"
+  security_groups    = var.sg_ids
   subnets            = var.subnet_ids
-  security_groups    = var.security_group_ids
-  tags = merge(var.tags, {
-    Environment = var.environment,
-    Name        = "${var.environment}-alb"
-  })
+  tags = {
+    Name = var.alb_name
+  }
 }
 
 resource "aws_lb_target_group" "this" {
-  name_prefix = "${var.environment}-tg"
-  port        = var.target_group_port
-  protocol    = var.target_group_protocol
-  vpc_id      = var.vpc_id
+  name     = var.alb_target_group_name
+  port     = var.target_group_port
+  protocol = var.target_group_protocol
+  vpc_id   = var.vpc_id
 
   health_check {
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    timeout             = 5
-    interval            = 30
+    healthy_threshold   = var.health_check_healthy_threshold
+    unhealthy_threshold = var.health_check_unhealthy_threshold
+    timeout             = var.health_check_timeout
+    interval            = var.health_check_interval
     path                = var.health_check_path
     matcher             = var.health_check_matcher
   }
 
-  tags = merge(var.tags, {
-    Environment = var.environment
-  })
+  tags = {
+    Name = var.alb_target_group_name
+  }
 }
 
 resource "aws_lb_listener" "this" {
@@ -42,20 +41,17 @@ resource "aws_lb_listener" "this" {
 }
 
 resource "aws_wafv2_web_acl" "this" {
-  name        = "${var.environment}-waf"
-  description = "WAF for ${var.environment} ALB"
+  name        = var.waf_name
+  description = "WAF for ${var.alb_name}"
   scope       = "REGIONAL"
-
   default_action {
     allow {}
   }
-
   visibility_config {
     cloudwatch_metrics_enabled = true
-    metric_name                = "${var.environment}-waf"
+    metric_name                = var.waf_name
     sampled_requests_enabled   = true
   }
-
   rule {
     name     = "RateLimitRule"
     priority = 1
@@ -70,7 +66,7 @@ resource "aws_wafv2_web_acl" "this" {
     }
     visibility_config {
       cloudwatch_metrics_enabled = true
-      metric_name                = "rate-limit"
+      metric_name                = "RateLimitRule"
       sampled_requests_enabled   = true
     }
   }
