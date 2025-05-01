08873f6f7ed401f11268b12e67896b7f16d5b0e3
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 01:18:59 +0530

    Update outputs.tf

--- terraform-project/modules/vpc/outputs.tf
+++ terraform-project/modules/vpc/outputs.tf
@@ -1,19 +1,15 @@
 output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.this.id
+  value = aws_vpc.this.id
 }
 
 output "public_subnet_ids" {
-  description = "IDs of the created public subnets"
-  value       = aws_subnet.public[*].id
-}
-
-output "private_subnet_ids" {
-  description = "IDs of the created private subnets"
-  value       = aws_subnet.private[*].id
+  value = aws_subnet.public[*].id
 }
 
 output "public_route_table_id" {
-  description = "The ID of the main public route table"
-  value       = aws_route_table.public.id
+  value = aws_route_table.public.id
+}
+
+output "vpc_cidr" {
+  value = var.vpc_cidr
 }
