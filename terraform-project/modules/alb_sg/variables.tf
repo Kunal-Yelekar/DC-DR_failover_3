418e3acdc336fa991683965e6103b64d74ed35e8
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 00:47:14 +0530

    Delete terraform-project/modules/alb_sg directory

--- terraform-project/modules/alb_sg/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  description = "Security group name for the ALB"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID in which to create the ALB security group"
-  type        = string
-}
