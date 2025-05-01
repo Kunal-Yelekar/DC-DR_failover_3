a67945f19a354fe74dc46c4b59594f6cb1f305df
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 00:40:18 +0530

    Update main.tf

--- terraform-project/modules/vpc/main.tf
+++ terraform-project/modules/vpc/main.tf
@@ -1,58 +1,91 @@
 resource "aws_vpc" "this" {
-  cidr_block           = var.cidr_block
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-
   tags = {
-    Name = var.vpc_name
+    Name        = var.vpc_name
+    Environment = var.environment
   }
 }
 
-resource "aws_internet_gateway" "igw" {
+resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
-
   tags = {
     Name = "${var.vpc_name}-igw"
   }
 }
 
 resource "aws_subnet" "public" {
-  count                   = length(var.public_subnet_cidrs)
+  count                   = length(var.public_subnets)
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnet_cidrs[count.index]
-  availability_zone       = var.availability_zones[count.index % length(var.availability_zones)]
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
-
   tags = {
-    Name = "${var.vpc_name}-public-${count.index + 1}"
+    Name = "${var.vpc_name}-public-${var.availability_zones[count.index]}"
   }
 }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.this.id
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+  tags = {
+    Name = "${var.vpc_name}-private-${var.availability_zones[count.index]}"
+  }
+}
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
   tags = {
     Name = "${var.vpc_name}-public-rt"
   }
 }
 
-resource "aws_route_table_association" "public_rt_assoc" {
+resource "aws_route_table_association" "public" {
   count          = length(aws_subnet.public)
   subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public_rt.id
+  route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route" "igw_route" {
-  route_table_id         = aws_route_table.public_rt.id
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.this.id
 }
 
-resource "aws_security_group" "alb_sg" {
-  name        = "${var.vpc_name}-alb-sg"
-  description = "Security group for ALB in ${var.vpc_name}"
-  vpc_id      = aws_vpc.this.id
+resource "aws_network_acl" "public" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "${var.vpc_name}-public-nacl"
+  }
+}
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
+resource "aws_network_acl_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  network_acl_id = aws_network_acl.public.id
+}
+
+resource "aws_network_acl_rule" "allow_http_inbound" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 80
+  to_port        = 80
+}
+
+resource "aws_network_acl_rule" "allow_all_outbound" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 0
+}
