71194d0e1831eb4b7b6ed6c7fd22fb14b564fa33
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 00:54:10 +0530

    Update backend.tf

--- terraform-project/backend.tf
+++ terraform-project/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-unique-terraform-state-bucket-knl2"
-    key            = var.remote_state_key
+    key            = "terraform/statefile"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
   }
