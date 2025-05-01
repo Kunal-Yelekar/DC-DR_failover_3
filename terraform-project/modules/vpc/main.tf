f34dc7b2cd56afcd340c9e4827a836e5584c5c6a
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 01:19:34 +0530

    Update main.tf

--- terraform-project/modules/vpc/main.tf
+++ terraform-project/modules/vpc/main.tf
@@ -3,8 +3,7 @@ resource "aws_vpc" "this" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name        = var.vpc_name
-    Environment = var.environment
+    Name = var.vpc_name
   }
 }
 
@@ -22,22 +21,18 @@ resource "aws_subnet" "public" {
   availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
   tags = {
-    Name = "${var.vpc_name}-public-${var.availability_zones[count.index]}"
-  }
-}
-
-resource "aws_subnet" "private" {
-  count             = length(var.private_subnets)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = var.private_subnets[count.index]
-  availability_zone = var.availability_zones[count.index]
-  tags = {
-    Name = "${var.vpc_name}-private-${var.availability_zones[count.index]}"
+    Name = "${var.vpc_name}-public-${count.index}"
   }
 }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
   tags = {
     Name = "${var.vpc_name}-public-rt"
   }
@@ -49,43 +44,31 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this.id
-}
-
-resource "aws_network_acl" "public" {
+resource "aws_network_acl" "this" {
   vpc_id = aws_vpc.this.id
   tags = {
-    Name = "${var.vpc_name}-public-nacl"
+    Name = "${var.vpc_name}-nacl"
   }
 }
 
-resource "aws_network_acl_association" "public" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
-  network_acl_id = aws_network_acl.public.id
-}
-
-resource "aws_network_acl_rule" "allow_http_inbound" {
-  network_acl_id = aws_network_acl.public.id
+resource "aws_network_acl_rule" "inbound_allow" {
+  network_acl_id = aws_network_acl.this.id
   rule_number    = 100
-  egress         = false
   protocol       = "tcp"
   rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-  from_port      = 80
-  to_port        = 80
-}
-
-resource "aws_network_acl_rule" "allow_all_outbound" {
-  network_acl_id = aws_network_acl.public.id
-  rule_number    = 100
-  egress         = true
-  protocol       = "-1"
-  rule_action    = "allow"
+  egress         = false
   cidr_block     = "0.0.0.0/0"
   from_port      = 0
-  to_port        = 0
+  to_port        = 65535
+}
+
+resource "aws_network_acl_rule" "outbound_allow" {
+  network_acl_id = aws_network_acl.this.id
+  rule_number    = 100
+  protocol       = "tcp"
+  rule_action    = "allow"
+  egress         = true
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 65535
 }
