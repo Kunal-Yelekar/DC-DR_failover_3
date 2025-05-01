3621357f3cf7a8ac6ab6b0bd5490e97dfc57bbb0
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 01:50:32 +0530

    Update outputs.tf

--- terraform-project/modules/global_accelerator/outputs.tf
+++ terraform-project/modules/global_accelerator/outputs.tf
@@ -1,7 +1,3 @@
-output "accelerator_arn" {
-  value = aws_globalaccelerator_accelerator.this.arn
-}
-
-output "accelerator_dns_name" {
-  value = aws_globalaccelerator_accelerator.this.dns_name
+output "accelerator_id" {
+  value = aws_globalaccelerator_accelerator.this.id
 }
