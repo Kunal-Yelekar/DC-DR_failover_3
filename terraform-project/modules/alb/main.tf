15ff4a3e4f2c1e33835c721faa2aa8679be808de
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 00:43:59 +0530

    Update main.tf

--- terraform-project/modules/alb/main.tf
+++ terraform-project/modules/alb/main.tf
@@ -1,19 +1,82 @@
 resource "aws_lb" "this" {
-  name               = var.name
+  name               = "${var.environment}-alb"
   internal           = false
-  load_balancer_type = var.load_balancer_type
-  security_groups    = var.sg_ids
-  subnets            = var.subnets
-  ip_address_type    = var.ip_address_type
-  tags = {
-    Name = var.name
+  load_balancer_type = "application"
+  subnets            = var.subnet_ids
+  security_groups    = var.security_group_ids
+  tags = merge(var.tags, {
+    Environment = var.environment,
+    Name        = "${var.environment}-alb"
+  })
+}
+
+resource "aws_lb_target_group" "this" {
+  name_prefix = "${var.environment}-tg"
+  port        = var.target_group_port
+  protocol    = var.target_group_protocol
+  vpc_id      = var.vpc_id
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    path                = var.health_check_path
+    matcher             = var.health_check_matcher
+  }
+
+  tags = merge(var.tags, {
+    Environment = var.environment
+  })
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = var.listener_port
+  protocol          = var.listener_protocol
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
   }
 }
 
-output "alb_arn" {
-  value = aws_lb.this.arn
+resource "aws_wafv2_web_acl" "this" {
+  name        = "${var.environment}-waf"
+  description = "WAF for ${var.environment} ALB"
+  scope       = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${var.environment}-waf"
+    sampled_requests_enabled   = true
+  }
+
+  rule {
+    name     = "RateLimitRule"
+    priority = 1
+    action {
+      block {}
+    }
+    statement {
+      rate_based_statement {
+        limit              = 2000
+        aggregate_key_type = "IP"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "rate-limit"
+      sampled_requests_enabled   = true
+    }
+  }
 }
 
-output "dns_name" {
-  value = aws_lb.this.dns_name
+resource "aws_wafv2_web_acl_association" "this" {
+  resource_arn = aws_lb.this.arn
+  web_acl_arn  = aws_wafv2_web_acl.this.arn
 }
