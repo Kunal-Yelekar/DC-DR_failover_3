3b4a3c374d544391a9d516d35db23f092ea9c3d5
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 00:21:45 +0530

    Update outputs.tf

--- terraform-project/outputs.tf
+++ terraform-project/outputs.tf
@@ -6,14 +6,14 @@ output "dr_vpc_id" {
   value = module.dr_vpc.vpc_id
 }
 
-output "alb_dns_name" {
-  value = module.alb.dns_name
+output "dc_alb_dns" {
+  value = module.dc_alb.alb_dns
 }
 
-output "dc_instance_id" {
-  value = module.dc_instance.instance_id
+output "dr_alb_dns" {
+  value = module.dr_alb.alb_dns
 }
 
-output "dr_instance_id" {
-  value = module.dr_instance.instance_id
+output "global_accelerator_dns" {
+  value = module.global_accelerator.accelerator_dns
 }
