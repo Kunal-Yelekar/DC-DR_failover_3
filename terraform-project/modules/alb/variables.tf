840bbb37813700581a1b3fa06fb3ce4e56acc5d2
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 00:44:14 +0530

    Update variables.tf

--- terraform-project/modules/alb/variables.tf
+++ terraform-project/modules/alb/variables.tf
@@ -1,26 +1,60 @@
-variable "name" {
-  description = "Name for the ALB"
+variable "environment" {
+  description = "Environment name, e.g., DC or DR"
   type        = string
 }
 
-variable "subnets" {
-  description = "Subnets in which the ALB will be deployed"
+variable "subnet_ids" {
+  description = "Subnets for ALB deployment"
   type        = list(string)
 }
 
-variable "sg_ids" {
-  description = "List of security group IDs for the ALB"
+variable "security_group_ids" {
+  description = "Security groups for ALB"
   type        = list(string)
 }
 
-variable "load_balancer_type" {
-  description = "The type of the load balancer (e.g., application)"
-  type        = string
-  default     = "application"
+variable "target_group_port" {
+  description = "Port for target group"
+  type        = number
 }
 
-variable "ip_address_type" {
-  description = "IP address type (ipv4 or dualstack)"
+variable "target_group_protocol" {
+  description = "Protocol for target group"
   type        = string
-  default     = "ipv4"
+  default     = "HTTP"
+}
+
+variable "health_check_path" {
+  description = "Path for health check"
+  type        = string
+  default     = "/"
+}
+
+variable "health_check_matcher" {
+  description = "Matcher for health check"
+  type        = string
+  default     = "200"
+}
+
+variable "listener_port" {
+  description = "Listener port for ALB"
+  type        = number
+  default     = 80
+}
+
+variable "listener_protocol" {
+  description = "Listener protocol for ALB"
+  type        = string
+  default     = "HTTP"
+}
+
+variable "vpc_id" {
+  description = "VPC ID for target group"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags applied to ALB and target group"
+  type        = map(string)
+  default     = {}
 }
