c84860257fb79f9907e20d2078e27e85b99f48d2
Kunal-Yelekar <132226091+Kunal-Yelekar@users.noreply.github.com>
2025-05-02 01:12:17 +0530

    Update outputs.tf

--- terraform-project/outputs.tf
+++ terraform-project/outputs.tf
@@ -1,24 +1,19 @@
 output "dc_vpc_id" {
-  description = "DC VPC ID"
-  value       = module.dc_vpc.vpc_id
+  value = module.dc_vpc.vpc_id
 }
 
 output "dr_vpc_id" {
-  description = "DR VPC ID"
-  value       = module.dr_vpc.vpc_id
+  value = module.dr_vpc.vpc_id
 }
 
-output "dc_alb_dns" {
-  description = "DC ALB DNS Name"
-  value       = module.dc_alb.alb_dns_name
+output "dc_alb_dns_name" {
+  value = module.dc_alb.alb_dns_name
 }
 
-output "dr_alb_dns" {
-  description = "DR ALB DNS Name"
-  value       = module.dr_alb.alb_dns_name
+output "dr_alb_dns_name" {
+  value = module.dr_alb.alb_dns_name
 }
 
-output "global_accelerator_dns" {
-  description = "Global Accelerator DNS Name"
-  value       = module.global_accelerator.ga_dns_name
+output "global_accelerator_dns_name" {
+  value = module.global_accelerator.accelerator_dns_name
 }
